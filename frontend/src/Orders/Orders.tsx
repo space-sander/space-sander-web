f72ec0be574e91177c87f6bba1f34c985c94785f
SpenceMitch <spencemitch13@yahoo.com>
2021-03-09 16:01:31 -0500

    added orders folder

---
+++ frontend/src/Orders/Orders.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import data from './data';
+
+function Orders() {
+    return (
+        <div className="content content-margined">
+            <div className="order-header">
+                <h3>Orders</h3>
+            </div>
+            <div className="order-list">
+                <table className="table">
+                    <thread>
+                        <tr>
+                            <th>ID</th>
+                            
+                        </tr>
+                    </thread>
+                </table>
+            </div>
+        </div>);
+};
+export default Orders;
